01e22149f6cc7f525f67c5934cee34e9a202673b
aQaTL <mmsoltys@outlook.com>
2020-12-01 08:13:30 +0100

    Day 1 optimization - skip already visited numbers

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,17 +6,31 @@ fn main() -> anyhow::Result<()> {
 
 	let part_1 = input
 		.iter()
-		.map(|a| (a, input.iter()))
-		.find_map(|(&a, mut b)| b.find(|&&b| b + a == 2020).map(|b| b * a))
+		.enumerate()
+		.map(|(a_idx, a)| ((a_idx, a), input.iter()))
+		.find_map(|((a_idx, &a), b)| b.skip(a_idx).find(|&&b| b + a == 2020).map(|b| b * a))
 		.unwrap();
 
 	println!("Part 1: {}", part_1);
 
 	let part_2 = input
 		.iter()
-		.map(|a| (a, input.iter().map(|b| (b, input.iter()))))
-		.find_map(|(&a, mut b)| {
-			b.find_map(|(&b, mut c)| c.find(|&&c| c + b + a == 2020).map(|c| c * b * a))
+		.enumerate()
+		.map(|(a_idx, a)| {
+			(
+				(a_idx, a),
+				input
+					.iter()
+					.enumerate()
+					.map(|(b_idx, b)| ((b_idx, b), input.iter())),
+			)
+		})
+		.find_map(|((a_idx, &a), b)| {
+			b.skip(a_idx).find_map(|((b_idx, &b), c)| {
+				c.skip(b_idx)
+					.find(|&&c| c + b + a == 2020)
+					.map(|c| c * b * a)
+			})
 		})
 		.unwrap();
 
