cce6c216ae67911c51c202b807480b310d443ca1
aQaTL <mmsoltys@outlook.com>
2020-12-09 06:36:03 +0100

    Day 9

---
+++ day_9/src/main.rs
@@ -0,0 +1,40 @@
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+	let input: Vec<usize> = std::fs::read_to_string("day_9/input.txt")?
+		.lines()
+		.map(str::parse)
+		.try_collect()?;
+
+	let preamble_size = 25;
+
+	let invalid_number = input
+		.iter()
+		.copied()
+		.enumerate()
+		.skip(preamble_size)
+		.find(|(idx, x)| {
+			!input[(idx - preamble_size)..=(idx - 1)]
+				.iter()
+				.copied()
+				.combinations(2)
+				.any(|comb| comb.into_iter().sum::<usize>() == *x)
+		})
+		.map(|(_, x)| x)
+		.ok_or_else(|| anyhow::anyhow!("Part 1 fail"))?;
+	println!("Part 1: {}", invalid_number);
+
+	let part_2 = (2..(input.len() / 2))
+		.into_iter()
+		.find_map(|i| {
+			input
+				.as_slice()
+				.windows(i)
+				.find(|x| x.iter().sum::<usize>() == invalid_number)
+				.and_then(|x| x.iter().minmax().into_option().map(|(min, max)| min + max))
+		})
+		.ok_or_else(|| anyhow::anyhow!("Part 2 fail"))?;
+	println!("Part 2: {}", part_2);
+
+	Ok(())
+}
