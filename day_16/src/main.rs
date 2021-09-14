ddc0ce72e8433703a7ab2ec63e3e1c8fd44bddea
aQaTL <mmsoltys@outlook.com>
2021-09-14 22:49:59 +0200

    Day 16 p2

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -5,12 +5,11 @@ use nom::combinator::{map, map_res};
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair};
 use nom::IResult;
-use std::collections::HashMap;
 use std::ops::RangeInclusive;
 
 fn main() -> Result<()> {
 	let input = std::fs::read_to_string("day_16/input.txt")?;
-	// println!("Part 1: {}", part_1(&input)?);
+	println!("Part 1: {}", part_1(&input)?);
 	println!("Part 2: {}", part_2(&input)?);
 	Ok(())
 }
@@ -35,62 +34,66 @@ fn part_1(input: &str) -> Result<u64> {
 
 fn part_2(input: &str) -> Result<u64> {
 	let document = parse_document(input)?;
-	dbg!(&document);
-	let mut product = 1;
-	let mut valid_tickets = Vec::new();
-	for nearby_ticket in &document.nearby_tickets {
-		for rule_value in &nearby_ticket.0 {
-			if document
-				.rules
-				.iter()
-				.any(|rule| rule.ranges.iter().any(|range| range.contains(rule_value)))
-			{
-				valid_tickets.push(nearby_ticket);
-			}
-		}
-	}
-	let mut mem = Vec::<(&Rule, u64, usize)>::new();
-	let mut visited = Vec::new();
 
-	'rules: for rule in &document.rules {
-		if !rule.name.starts_with("departure") {
-			// continue;
-		}
-		eprintln!("visiting {:?}", rule);
-		'rule_idxs: for rule_idx in 0..document.rules.len(){
-			if visited.contains(&rule_idx) {
-				eprintln!("skipping {}", rule_idx);
-				continue;
-			}
-			let mut found = true;
-			for nearby_ticket in &valid_tickets {
-				let nearby_ticket_rule =  nearby_ticket.0[rule_idx];
-				eprintln!("testing {}", nearby_ticket_rule);
-				if !rule.ranges.iter().any(|range| range.contains(&nearby_ticket_rule)) {
-					eprintln!("false", );
-					found = false;
-					break;
+	let mut ticket_fields = Vec::<Vec<u64>>::new();
+
+	for field_idx in 0..document.rules.len() {
+		'tickets: for ticket in &document.nearby_tickets {
+			for rule_value in &ticket.0 {
+				if !document
+					.rules
+					.iter()
+					.any(|rule| rule.ranges.iter().any(|range| range.contains(rule_value)))
+				{
+					continue 'tickets;
 				}
 			}
-			// if !rule.ranges.iter().any(|range| range.contains(&document.my_ticket.0[rule_idx])) {
-			// 	eprintln!("false", );
-			// 	found = false;
-			// 	break;
-			// }
-
-			if found {
-				// if rule.name.starts_with("departure") {
-					// product *= document.my_ticket.0[rule_idx];
-					product += 1;
-				// }
-				visited.push(rule_idx);
-				eprintln!("{:?} matches on idx {}", rule, rule_idx);
-				continue 'rules;
+			match ticket_fields.get_mut(field_idx) {
+				Some(v) => v.push(ticket.0[field_idx]),
+				None => {
+					ticket_fields.push(vec![ticket.0[field_idx]]);
+				}
 			}
 		}
 	}
 
-	eprintln!("{:#?}", mem);
+	let mut product = 1;
+
+	let mut visited = Vec::<usize>::new();
+	let mut visited_field = Vec::<usize>::new();
+
+	while visited.len() != document.rules.len() {
+		for (rule_idx, rule) in document.rules.iter().enumerate() {
+			if visited.contains(&rule_idx) {
+				continue;
+			}
+
+			let mut matching_fields_count = 0;
+			let mut last_matching_idx = 0;
+
+			for (idx, columns) in ticket_fields.iter().enumerate() {
+				if visited_field.contains(&idx) {
+					continue;
+				}
+
+				let matches = columns
+					.iter()
+					.all(|column| rule.ranges.iter().any(|range| range.contains(column)));
+
+				if matches {
+					matching_fields_count += 1;
+					last_matching_idx = idx;
+				}
+			}
+			if matching_fields_count == 1 {
+				visited.push(rule_idx);
+				visited_field.push(last_matching_idx);
+				if rule.name.starts_with("departure") {
+					product *= document.my_ticket.0[last_matching_idx];
+				}
+			}
+		}
+	}
 
 	Ok(product)
 }
@@ -111,14 +114,14 @@ struct Rule<'a> {
 #[derive(Debug)]
 struct Ticket(Vec<u64>);
 
-fn parse_document<'a>(input: &'a str) -> Result<Document<'a>> {
+fn parse_document(input: &str) -> Result<Document> {
 	match parse_document_nom(input) {
 		Ok((_, document)) => Ok(document),
 		Err(e) => Err(anyhow::anyhow!("{:?}", e)),
 	}
 }
 
-fn parse_document_nom<'a>(input: &'a str) -> IResult<&'a str, Document<'a>> {
+fn parse_document_nom(input: &str) -> IResult<&str, Document> {
 	let (input, rules) = separated_list1(multispace1, parse_rule)(input)?;
 
 	let (input, _) = preceded(multispace0, tag("your ticket:\n"))(input)?;
@@ -137,7 +140,7 @@ fn parse_document_nom<'a>(input: &'a str) -> IResult<&'a str, Document<'a>> {
 	))
 }
 
-fn parse_rule<'a>(input: &'a str) -> IResult<&'a str, Rule<'a>> {
+fn parse_rule(input: &str) -> IResult<&str, Rule> {
 	let (input, name) = take_until1(": ")(input)?;
 	let (input, _) = tag(": ")(input)?;
 	let (input, ranges) = separated_list1(tag(" or "), parse_range)(input)?;
@@ -145,12 +148,12 @@ fn parse_rule<'a>(input: &'a str) -> IResult<&'a str, Rule<'a>> {
 }
 
 fn parse_range(input: &str) -> IResult<&str, RangeInclusive<u64>> {
-	Ok(map_res(
+	map_res(
 		separated_pair(digit1, char('-'), digit1),
 		|(a, b): (&str, &str)| -> Result<RangeInclusive<u64>, std::num::ParseIntError> {
 			Ok(a.parse::<u64>()?..=b.parse::<u64>()?)
 		},
-	)(input)?)
+	)(input)
 }
 
 fn parse_ticket(input: &str) -> IResult<&str, Ticket> {
